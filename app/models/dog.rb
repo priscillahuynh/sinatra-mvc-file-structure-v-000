e4459248dc75cc6fe45937cc69e2a714ea497528
Priscilla H <priscillamhuynh@gmail.com>
2019-02-01 07:52:19 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,11 @@
 class Dog 
   attr_accessor :name, :breed, :age 
-  @@all = [ ]
+  @@all = []
   def initialize(name, breed, age)
   @name = name 
   @breed = breed
   @age = age
+  
   end
   
 end
